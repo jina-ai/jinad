1da4633ffbfeae69321a7da9b76032862fd09314
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2020-10-15 04:48:29 +0530

    fix: changing localhost

--- jinad/main.py
+++ jinad/main.py
@@ -13,16 +13,20 @@ app = FastAPI(
     description='REST API for creating / deleting Jina Flow',
     version='0.1.0'
 )
+# TODO: handle prefix
 app.include_router(router=api_router, prefix='/v1')
 
 
 if __name__ == "__main__":
+    # TODO: refactor main
     config = Config()
-    config.bind = ["localhost:8000"] 
+    # TODO: to be parameterized
+    config.bind = ["0.0.0.0:8000"] 
     
     asyncio.set_event_loop_policy(
         uvloop.EventLoopPolicy()
     )
+    # TODO: add shutdown handler
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(
