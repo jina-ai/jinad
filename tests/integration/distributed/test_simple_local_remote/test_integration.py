1e00ecb8e76ce007a243348924d7fb3b18aa737c
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-12-30 16:55:48 +0100

    test: improve testing, problem mapping paths

--- tests/integration/distributed/test_simple_local_remote/test_integration.py
+++ tests/integration/distributed/test_simple_local_remote/test_integration.py
@@ -1,74 +1,28 @@
 import os
-from pathlib import Path
+
 import pytest
-import sys
-import shutil
-import time
+from jina.flow import Flow
 
-from jina import Flow
-from tests.helpers import (
-    start_docker_compose,
-    stop_docker_compose,
-    send_flow,
-    call_api,
-    get_results,
-)
-
-directory = Path('tests/integration/distributed/test_simple_local_remote/')
-compose_yml = directory / 'docker-compose.yml'
-flow_yml = directory / 'flow.yml'
-pod_dir = directory / 'pods'
-
-workspace = Path('workspace')
-if workspace.exists():
-    shutil.rmtree(workspace)
-
-def test_flow():
-    if Path.cwd().name != 'jinad':
-        sys.exit(
-            'test_integration.py should only be run from the jinad base directory'
-        )
-
-    os.environ['JINA_ENCODER_HOST'] = '172.20.128.2'
-    os.environ['JINA_INDEXER_HOST'] = '172.20.128.3'
-
-    start_docker_compose(compose_yml)
-
-    time.sleep(10)
-
-    with Flow.load_config(str(flow_yml)) as f:
-        f.index_lines(['text:cats rulessss'])
-    
-    # f = Flow().load_config(str(flow_yml))
-    # f.use_rest_gateway()
-    # with f:
-    #     text_indexed = call_api(
-    #     method='post',
-    #     url='http://0.0.0.0:45678/api/search',
-    #     payload={'top_k': 10, 'data': ['text:cat']},
-    # )['search']['docs'][0]['text']
-
-    # print(text_indexed)
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+compose_yml = os.path.join(cur_dir, 'docker-compose.yml')
+flow_yml = os.path.join(cur_dir, 'flow.yml')
+pod_dir = os.path.join(cur_dir, 'pods')
 
 
-    # flow_id = send_flow(flow_yml, pod_dir)['flow_id']
+@pytest.mark.parametrize('docker_compose', [compose_yml], indirect=['docker_compose'])
+def test_flow(docker_compose, mocker):
+    text = 'cats rules'
+    m = mocker.Mock()
 
-    # print(f'Successfully started the flow: {flow_id}. Lets index some data')
+    def validate_output(resp):
+        m()
+        assert len(resp.index.docs) == 1
+        assert resp.index.docs[0].text == text
 
-    # text_indexed = call_api(
-    #     method='post',
-    #     url='http://0.0.0.0:45678/api/search',
-    #     payload={'top_k': 10, 'data': ['text:cats rulessss']},
-    # )['search']['docs'][0]['text']
+    os.environ['JINA_ENCODER_HOST'] = '172.28.1.1'
+    os.environ['JINA_INDEXER_HOST'] = '172.28.1.2'
 
-    # assert text_indexed == 'text:cats rulessss'
+    with Flow.load_config(flow_yml) as f:
+        f.index_lines([text], on_done=validate_output)
 
-    # r = call_api(method='get', url=f'http://localhost:8000/v1/flow/{flow_id}')
-    # assert r['status_code'] == 200
-
-    # r = call_api(
-    #     method='delete', url=f'http://localhost:8000/v1/flow?flow_id={flow_id}'
-    # )
-    # assert r['status_code'] == 200
-
-    # stop_docker_compose(compose_yml)
+    m.assert_called_once()
